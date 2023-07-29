dfb6b986191b981748d6cea9c7e6d531befbbf5b
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-07-29 19:13:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/manavahir902/Auto-search-tamil-bot ok && cd ok && pip3 install -U -r requirements.txt
+python3 bot.py
